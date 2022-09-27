4ff85a0d60b94e8295733dbd8deba8572455250f
jiyseo <jji5602@gmail.com>
2022-09-27 19:13:08 +0900

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // 기본 양식 const = 변수선언
+const httpd = require("http");
 const app = express(); // 기본 양식
 app.use(express.urlencoded({ extended: true }));
-const MongoClient = require("mongodb").MongoClient; //npm install mongodb
 app.set("view engine", "ejs"); //ejs 사용 양식
 const methodOverride = require("method-override"); //PUT 메소드 사용
 app.use(methodOverride("_method")); //PUT 메소드 사용
@@ -10,26 +10,19 @@ require("dotenv").config();
 const moment = require("moment"); // moment method
 const { query } = require("express");
 
-const createHashedPassword = (password) => {
-  return crypto.createHash("sha512").update(password).digest("base64");
-};
-
 app.use("/public", express.static("public")); //static 파일(css) 보관하기 위해 public폴더를 쓸 것이다
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "leteatgo.cuom1ib1jx9z.ap-northeast-2.rds.amazonaws.com", // 호스트 주소
+var db_info = {
+  host: "localhost", // 호스트 주소
+  port: 3306,
   user: "admin", // mysql user
   password: process.env.DB_PASSWORD, // mysql password
   database: "leteatgo", // mysql 데이터베이스
-});
+};
+var connection = mysql.createConnection(db_info);
 connection.connect();
 
-app.listen(process.env.PORT, function () {
-  console.log("listening on 3306");
-});
-
-var logcheck = 0;
 app.get("/survey", function (req, res) {
   if (!req.query.id) {
     res.render("survey.ejs");
